9a55ac69d1cc6431ce90e5a9df9b0cdd05d4f59c
Boris Ivis <boris.ivis@hotmail.com>
2026-09-22 13:22:03 +0200

    oanvända klasser borttagna

--- src/main/java/Pensionaten/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-/*package Pensionaten.repositories;
-
-import Pensionaten.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Repository för kunddata
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}*/
