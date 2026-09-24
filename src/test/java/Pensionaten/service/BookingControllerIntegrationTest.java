8ba6795ae6565aa53d8c97c98119cf2f8bbb75e6
Boris Ivis <boris.ivis@hotmail.com>
2026-09-24 05:53:52 +0200

    Another test for deployment and CI

--- src/test/java/Pensionaten/service/BookingControllerIntegrationTest.java
+++ src/test/java/Pensionaten/service/BookingControllerIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//Test auto deploy and ci actions
 @SpringBootTest
 @AutoConfigureMockMvc
 class BookingControllerIntegrationTest {
